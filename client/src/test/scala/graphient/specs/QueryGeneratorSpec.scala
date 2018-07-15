f82a00e862b0a08df2580ba5b0828a4389e1941b
erdeszt <erdeszt@gmail.com>
2018-07-15 23:18:11 +0200

    Add invalid query specs

--- client/src/test/scala/graphient/specs/QueryGeneratorSpec.scala
+++ client/src/test/scala/graphient/specs/QueryGeneratorSpec.scala
@@ -11,6 +11,43 @@ class QueryGeneratorSpec extends FunSpec with Matchers {
 
   describe("QueryGenerator") {
 
+    describe("call by name api") {
+
+      it("should generate a valid query ast for queries") {
+        val queryAst = queryGenerator.generateQuery(QueryByName("getUser"))
+
+        queryAst should be('right)
+
+        val violations = QueryValidator.default.validateQuery(TestSchema.schema, queryAst.right.toOption.get)
+
+        violations shouldBe empty
+      }
+
+      it("should generate a valid query ast for mutations") {
+        val queryAst = queryGenerator.generateQuery(MutationByName("createUser"))
+
+        queryAst should be('right)
+
+        val violations = QueryValidator.default.validateQuery(TestSchema.schema, queryAst.right.toOption.get)
+
+        violations shouldBe empty
+      }
+
+      it("should not allow queries to be called as mutations") {
+        val queryAst = queryGenerator.generateQuery(QueryByName("createUser"))
+
+        queryAst should be('left)
+        queryAst should be(Left(FieldNotFound(QueryByName("createUser"))))
+      }
+
+      it("should not allow mutations to be called as queries") {
+        val queryAst = queryGenerator.generateQuery(MutationByName("getUser"))
+
+        queryAst should be('left)
+        queryAst should be(Left(FieldNotFound(MutationByName("getUser"))))
+      }
+    }
+
     describe("call by field api") {
 
       it("should generate a valid query ast for queries") {
@@ -33,10 +70,18 @@ class QueryGeneratorSpec extends FunSpec with Matchers {
         violations shouldBe empty
       }
 
-      ignore("should not allow queries to be called as mutations") {
-        intercept {
-          queryGenerator.generateQuery(Query(TestSchema.Mutations.createUser))
-        }
+      it("should not allow queries to be called as mutations") {
+        val queryAst = queryGenerator.generateQuery(Query(TestSchema.Mutations.createUser))
+
+        queryAst should be('left)
+        queryAst should be(Left(FieldNotFound(QueryByName("createUser"))))
+      }
+
+      it("should not allow mutations to be called as queries") {
+        val queryAst = queryGenerator.generateQuery(Mutation(TestSchema.Queries.getUser))
+
+        queryAst should be('left)
+        queryAst should be(Left(FieldNotFound(MutationByName("getUser"))))
       }
 
     }
