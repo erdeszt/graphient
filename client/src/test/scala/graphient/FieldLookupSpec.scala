3ccfc4626e359520f8f41b24f618af99d7b54d56
erdeszt <erdeszt@gmail.com>
2018-07-15 22:44:16 +0200

    Implement FieldLookup specs

--- client/src/test/scala/graphient/FieldLookupSpec.scala
+++ client/src/test/scala/graphient/FieldLookupSpec.scala
@@ -4,14 +4,46 @@ import org.scalatest._
 
 class FieldLookupSpec extends FunSpec with Matchers {
 
+  object TestObject extends FieldLookup
+
   describe("FieldLookup") {
 
-    ignore("should find existing fields") {
-      fail("WIP")
+    it("should find existing query fields") {
+      val lookupResult = TestObject.getField(TestSchema.schema, QueryByName("getUser"))
+
+      lookupResult should be('right)
+      lookupResult.right.toOption.get.name should be("getUser")
     }
 
-    ignore("should report missing fields") {
-      fail("WIP")
+    it("should report missing query fields") {
+      val lookupResult = TestObject.getField(TestSchema.schema, QueryByName("undefined"))
+
+      lookupResult should be(Left(FieldNotFound(QueryByName("undefined"))))
+    }
+
+    it("should find existing mutation fields") {
+      val lookupResult = TestObject.getField(TestSchema.schema, MutationByName("createUser"))
+
+      lookupResult should be('right)
+      lookupResult.right.toOption.get.name should be("createUser")
+    }
+
+    it("should report missing mutation fields") {
+      val lookupResult = TestObject.getField(TestSchema.schema, MutationByName("undefined"))
+
+      lookupResult should be(Left(FieldNotFound(MutationByName("undefined"))))
+    }
+
+    it("should not lookup a query when a mutation is searched") {
+      val lookupResult = TestObject.getField(TestSchema.schema, MutationByName("getUser"))
+
+      lookupResult should be(Left(FieldNotFound(MutationByName("getUser"))))
+    }
+
+    it("should not lookup a mutation when a query is searched") {
+      val lookupResult = TestObject.getField(TestSchema.schema, QueryByName("createUser"))
+
+      lookupResult should be(Left(FieldNotFound(QueryByName("createUser"))))
     }
 
   }
