407181a6e2b4e62fa46d44829e904455fee9f076
erdeszt <erdeszt@gmail.com>
2019-03-22 09:30:14 +0100

    Fake Map[Any, String] encoder

--- graphienttp/src/main/scala/graphienttp/Main.scala
+++ graphienttp/src/main/scala/graphienttp/Main.scala
@@ -2,12 +2,16 @@ package graphienttp
 import com.softwaremill.sttp.{HttpURLConnectionBackend, Id, SttpBackend}
 import graphient.{Query, TestSchema}
 import graphient.TestSchema.Domain.UserRepo
-
 import com.softwaremill.sttp._
+import io.circe._
 
 object Main {
 
   implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
+
+  implicit val mapStringAnyEncoder: Encoder[Map[String, Any]] = { _ =>
+    Json.fromFields(List("userId" -> Json.fromLong(1L)))
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -17,7 +21,7 @@ object Main {
 
     // TODO: Map[String, Any] based implementation for convenience
     val request: Request[String, Nothing] =
-      client.runQuery(Query(TestSchema.Queries.getUser), GetUserPayload(42L))
+      client.runQuery(Query(TestSchema.Queries.getUser), Map[String, Any]("userId" -> 1L)) // GetUserPayload(42L))
 
     val response = request.send()
 
