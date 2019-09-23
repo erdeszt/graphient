6ea8c9bbd29dc4ba400faa28867694a9a37b8787
erdeszt <erdeszt@gmail.com>
2019-09-23 12:53:28 +0200

    Unwrap query field name in response

--- graphient/src/main/scala/graphient/GraphientClient.scala
+++ graphient/src/main/scala/graphient/GraphientClient.scala
@@ -4,7 +4,8 @@ import cats.effect.Sync
 import cats.implicits._
 import io.circe.parser.decode
 import com.softwaremill.sttp._
-import io.circe.{Decoder, Encoder}
+import io.circe.Decoder.Result
+import io.circe.{Decoder, Encoder, HCursor}
 import sangria.renderer.QueryRenderer
 import sangria.schema.Schema
 
@@ -29,19 +30,34 @@ class GraphientClient(schema: Schema[_, _], endpoint: Uri) {
     }
   }
 
+  case class DataWrapper[T](fieldName: String, value: T)
+
+  def dataWrapperDecoder[T: Decoder](fieldName: String): Decoder[DataWrapper[T]] = new Decoder[DataWrapper[T]] {
+    def apply(c: HCursor): Result[DataWrapper[T]] = {
+      val field   = c.downField(fieldName)
+      val payload = implicitly[Decoder[T]].tryDecode(field)
+
+      payload.map(DataWrapper(fieldName, _))
+    }
+  }
+
   def call[F[_], P: Encoder, T: Decoder](
-      call:             GraphqlCall[_, _],
-      variables:        P,
-      headers:          (String, String)*
-  )(implicit backend:   SttpBackend[F, _], effect: Sync[F]): F[T] = {
+      call:           GraphqlCall[_, _],
+      variables:      P,
+      headers:        (String, String)*
+  )(implicit backend: SttpBackend[F, _], effect: Sync[F]): F[T] = {
     for {
-      request <- createRequest(call, variables, headers:_*) match {
+      request <- createRequest(call, variables, headers: _*) match {
         case Left(error)         => effect.raiseError[Request[String, Nothing]](error)
         case Right(requestValue) => effect.pure(requestValue)
       }
       rawResponse <- request.send()
-      rawResponseBody     = rawResponse.body.leftMap(GraphqlClientError)
-      decodedResponseBody = rawResponseBody.flatMap(decode[RawGraphqlResponse[T]])
+      rawResponseBody = rawResponse.body.leftMap(GraphqlClientError)
+      decodedResponseBody = rawResponseBody.flatMap { body =>
+        decode[RawGraphqlResponse[DataWrapper[T]]](body)(
+          RawGraphqlResponse.graphqlResponseDecoder(dataWrapperDecoder[T](call.field.name))
+        )
+      }
       response <- decodedResponseBody match {
         case Left(error) => effect.raiseError[T](error)
         case Right(response) =>
@@ -52,7 +68,7 @@ class GraphientClient(schema: Schema[_, _], endpoint: Uri) {
               effect.raiseError[T](GraphqlClientError("Error fields is present but empty"))
             case (Some(firstError :: _), _) =>
               effect.raiseError[T](firstError)
-            case (None, Some(data)) => effect.pure(data)
+            case (None, Some(data)) => effect.pure(data.value)
           }
       }
     } yield response
