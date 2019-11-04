c46ed2523fc3de6e2a24c399fdf3a712d12e6482
erdeszt <erdeszt@gmail.com>
2019-11-04 08:50:32 +0100

    Remove unused dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,20 +7,6 @@ object Dependencies {
     "org.sangria-graphql" %% "sangria-circe" % "1.2.1"
   )
 
-  private lazy val circeVersion = "0.12.1"
-  lazy val circe = Seq(
-    "io.circe" %% "circe-core" % circeVersion,
-    "io.circe" %% "circe-generic" % circeVersion,
-    "io.circe" %% "circe-parser" % circeVersion
-  )
-  lazy val circeTest = Seq(
-    "io.circe" %% "circe-optics" % "0.12.0"
-  ).map(_ % Test)
-
-  lazy val spray = Seq(
-    "io.spray" %% "spray-json" % "1.3.2"
-  )
-
   lazy val cats = Seq(
     "org.typelevel" %% "cats-core" % "2.0.0",
     "org.typelevel" %% "cats-effect" % "2.0.0"
@@ -33,6 +19,17 @@ object Dependencies {
   lazy val scalaTest = Seq(
     "org.scalatest" %% "scalatest" % "3.0.5",
     "org.scalacheck" %% "scalacheck" % "1.14.1"
+  )
+
+  private lazy val circeVersion = "0.12.1"
+  lazy val circe = Seq(
+    "io.circe" %% "circe-core" % circeVersion,
+    "io.circe" %% "circe-generic" % circeVersion,
+    "io.circe" %% "circe-parser" % circeVersion
+  )
+
+  lazy val spray = Seq(
+    "io.spray" %% "spray-json" % "1.3.2"
   )
 
 }
