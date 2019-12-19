5b347ee854afde3bc1e1ec19b15e0ef89c17bbb2
erdeszt <erdeszt@gmail.com>
2019-12-19 11:29:35 +0100

    Add params converter for spray

--- graphient-spray/src/main/scala/graphient/serializer/spray.scala
+++ graphient-spray/src/main/scala/graphient/serializer/spray.scala
@@ -18,9 +18,43 @@ object spray extends DefaultJsonProtocol {
     }
   }
 
+  private def convertValue(value: Any): JsValue = {
+    value match {
+      case str: String =>
+        JsString(str)
+      case float: Float =>
+        JsNumber(float)
+      case double: Double =>
+        JsNumber(double)
+      case int: Int =>
+        JsNumber(int)
+      case bigInt: BigInt =>
+        JsNumber(bigInt)
+      case long: Long =>
+        JsNumber(long)
+      case option: Option[_] =>
+        option match {
+          case None             => JsNull
+          case Some(innerValue) => convertValue(innerValue)
+        }
+      case list: List[_] =>
+        JsArray(list.map(convertValue).toVector)
+      case array: Array[_] =>
+        JsArray(array.map(convertValue).toVector)
+      case obj: Map[_, _] =>
+        val mappedValues = obj.mapValues(convertValue).asInstanceOf[Map[String, JsValue]]
+        JsObject(mappedValues)
+    }
+  }
+
   implicit def graphqlRequestSprayFormat[T: JsonFormat] = jsonFormat2(GraphqlRequest[T](_, _))
   implicit val graphqlResponseErrorLocationSprayForamt = jsonFormat2(GraphqlResponseErrorLocation(_, _))
   implicit val graphqlResponseErrorSprayFormat         = jsonFormat3(GraphqlResponseError(_, _, _))
   implicit def rawGraphqlResponseSprayFormat[T: JsonFormat] = jsonFormat2(RawGraphqlResponse[T](_, _))
+  implicit val mapOfStringToAnySprayEncoder = new JsonWriter[Map[String, Any]] {
+    def write(obj: Map[String, Any]): JsValue = {
+      JsObject(obj.mapValues(convertValue))
+    }
+  }
 
 }
